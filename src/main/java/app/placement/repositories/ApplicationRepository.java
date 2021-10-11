4ed537a3874dcf1e95efdcfa4c147c6a9515f378
Aravind Pidaparthy <aravind170701@gmail.com>
2021-10-11 20:36:35 +0530

    issue

--- src/main/java/app/placement/repositories/ApplicationRepository.java
+++ src/main/java/app/placement/repositories/ApplicationRepository.java
@@ -1,7 +1,5 @@
 package app.placement.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.placement.dao.Application;
@@ -9,7 +7,4 @@ import app.placement.dao.Application;
 
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 
-	List<Application> findAllByCompanyBranchOrderByNotificationIdDesc(String branch);
-
-	List<Application> findAllByOrderByNotificationIdDesc();
 }
