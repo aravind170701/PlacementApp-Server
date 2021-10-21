e5dfd0ba4a2444b91878fa60682b9229195ec2f1
Aravind Pidaparthy <aravind170701@gmail.com>
2021-10-22 00:31:24 +0530

    Some Changes

--- src/main/java/app/placement/dao/User.java
+++ src/main/java/app/placement/dao/User.java
@@ -69,6 +69,6 @@ public class User implements Serializable {
 	@Column
 	private String cvUrl;
 
-	// @OneToMany(mappedBy = "user")
-	// private Set<Application> applications;
+	@OneToMany(mappedBy = "user")
+	private Set<Application> applications;
 }
