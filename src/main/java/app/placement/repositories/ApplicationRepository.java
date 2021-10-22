1ba5f076b3002f728e2f8c725a3c7ac3c1c3173b
unknown <nishantduttmishra@gmail.com>
2021-10-22 19:28:14 +0530

    made changes

--- src/main/java/app/placement/repositories/ApplicationRepository.java
+++ src/main/java/app/placement/repositories/ApplicationRepository.java
@@ -9,6 +9,7 @@ import app.placement.dao.Application;
 
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 
+	@Query("SELECT * FROM application_details where prn = ?1 and notification_id = ?2")
 	public Optional<Application> findAllByPrnAndNotificationId(String prn, Integer notificationId);
 
 	public List<Application> findByNotificationId(Integer notificationId);
