86e079b1f8253260f96285ddac4f8854c905a920
unknown <nishantduttmishra@gmail.com>
2021-10-22 14:21:09 +0530

    made changes

--- src/main/java/app/placement/dao/Notification.java
+++ src/main/java/app/placement/dao/Notification.java
@@ -39,6 +39,6 @@ public class Notification implements Serializable {
 	@Column
 	private String companyDescription;
 
-	@OneToMany(targetEntity = Application.class, mappedBy = "company_notifications")
+	@OneToMany(targetEntity = Application.class, mappedBy = "notification")
 	private Set<Application> applications;
 }
