e3e3e3686cacc4e5e140a7004ee8263207792b7b
unknown <nishantduttmishra@gmail.com>
2021-10-22 19:42:44 +0530

    made changes

--- src/main/java/app/placement/repositories/ApplicationRepository.java
+++ src/main/java/app/placement/repositories/ApplicationRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 
-	@Query("SELECT * FROM application_details where prn = ?1 and notification_id = ?2")
+	@Query("FROM Application a where a.user.prn = ?1 and a.notification.notificationId = ?2")
 	public Optional<Application> findAllByPrnAndNotificationId(String prn, Integer notificationId);
 
 	public List<Application> findByNotificationId(Integer notificationId);
